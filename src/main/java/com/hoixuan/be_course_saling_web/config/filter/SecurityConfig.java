18ef6758818c420098c1ae24e28460d742e0b2cb
Conganh97 <anhbc.afsystem@gmail.com>
2022-09-06 10:43:44 +0700

    toke

--- src/main/java/com/hoixuan/be_course_saling_web/config/filter/SecurityConfig.java
+++ src/main/java/com/hoixuan/be_course_saling_web/config/filter/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/auth/**").permitAll()
-                .and().authorizeRequests().antMatchers("/user/**","/course","/wallet").hasRole("USER")
+                .and().authorizeRequests().antMatchers("/user/**","/course","/wallet").hasAnyRole("ADMIN","USER")
                 .and().authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
                 .and().csrf().disable();
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
