d5679b6ac6d8d8d26cdf57395750e0d0ecddd927
Conganh97 <anhbc.afsystem@gmail.com>
2022-09-10 22:41:24 +0700

    recharge update

--- src/main/java/com/hoixuan/be_course_saling_web/controller/usercontroller/WalletAPI.java
+++ src/main/java/com/hoixuan/be_course_saling_web/controller/usercontroller/WalletAPI.java
@@ -1,10 +1,12 @@
 package com.hoixuan.be_course_saling_web.controller.usercontroller;
 
 import com.hoixuan.be_course_saling_web.model.AppUser;
+import com.hoixuan.be_course_saling_web.model.Bill;
 import com.hoixuan.be_course_saling_web.model.RequestRecharge;
 import com.hoixuan.be_course_saling_web.model.Wallet;
 import com.hoixuan.be_course_saling_web.model.dto.Recharge;
 import com.hoixuan.be_course_saling_web.service.AppUserService;
+import com.hoixuan.be_course_saling_web.service.BillService;
 import com.hoixuan.be_course_saling_web.service.RequestRechargeService;
 import com.hoixuan.be_course_saling_web.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.Calendar;
 
 @RestController
 @CrossOrigin("*")
@@ -23,12 +28,25 @@ public class WalletAPI {
     @Autowired
     AppUserService appUserService;
     @Autowired
+    BillService billService;
+    @Autowired
     RequestRechargeService requestRechargeService;
     @PostMapping("/recharge")
     public ResponseEntity<Wallet> recharge(@RequestBody Recharge recharge){
         Wallet wallet = walletService.findByIdUser(recharge.getIdUser());
         wallet.setMoney(wallet.getMoney() + recharge.getMoney());
         requestRechargeService.delete(recharge.getIdReq());
+        Bill bill = new Bill();
+        bill.setPaymentMethod("Banking");
+        bill.setContentBill("Recharge");
+        bill.setTotalBill(recharge.getMoney());
+        bill.setStatus(true);
+        Calendar cal = Calendar.getInstance();
+        Date date=new Date(cal.getTimeInMillis());
+        bill.setCreateAt(date);
+        AppUser appUser = appUserService.findById(recharge.getIdUser());
+        bill.setAppUser(appUser);
+        billService.save(bill);
         return new ResponseEntity<>(walletService.save(wallet), HttpStatus.OK);
     }
 }
